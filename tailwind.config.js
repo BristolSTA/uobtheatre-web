693d35c77e0640b7431b3b4ee89d6182a4c91ffe
Alex Toff <me@alextoff.uk>
2020-11-11 17:35:13 +0000

    build: Implement colour palette and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        sta: {
+          gray: {
+            regular: '#2B303A',
+            dark: '#23272A',
+            light: '#707070',
+          },
+          rouge: '#FF6978',
+          green: '#FF6978',
+          orange: '#FF9F1C',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
