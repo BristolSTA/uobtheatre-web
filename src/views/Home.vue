9b22c99aca2a0840a42b3765f17af4e492cfa739
Alex Toff <me@alextoff.uk>
2020-11-15 00:19:35 +0000

    Get rid of redundant outer div

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="home">
-    <div>
-      <div
-        id="splashscreen"
-        :style="{
-          'background-image': splashBackground,
-        }"
-      >
-        <div class="bg-black bg-opacity-40 flex items-center">
-          <div class="text-white container px-4 lg:w-2/3">
-            <template v-if="productions.length > 0">
-              <div class="text-2xl">{{ productions[0].society.name }}</div>
-              <div class="text-4xl">{{ productions[0].name }}</div>
-              <div class="text-2xl">
-                {{ productions[0].start_date | dateFormat('d MMMM') }} -
-                {{ productions[0].end_date | dateFormat('d MMMM y') }}
-              </div>
-            </template>
-            <template v-else>
-              <div class="text-4xl">Welcome to UOB Theatre</div>
-              <div class="text-2xl">
-                The Home of Bristol Student Perfomring Arts
-              </div>
-            </template>
-          </div>
+    <div
+      id="splashscreen"
+      :style="{
+        'background-image': splashBackground,
+      }"
+    >
+      <div class="bg-black bg-opacity-40 flex items-center">
+        <div class="text-white container px-4 lg:w-2/3">
+          <template v-if="productions.length > 0">
+            <div class="text-2xl">{{ productions[0].society.name }}</div>
+            <div class="text-4xl">{{ productions[0].name }}</div>
+            <div class="text-2xl">
+              {{ productions[0].start_date | dateFormat('d MMMM') }} -
+              {{ productions[0].end_date | dateFormat('d MMMM y') }}
+            </div>
+          </template>
+          <template v-else>
+            <div class="text-4xl">Welcome to UOB Theatre</div>
+            <div class="text-2xl">
+              The Home of Bristol Student Perfomring Arts
+            </div>
+          </template>
         </div>
       </div>
     </div>
