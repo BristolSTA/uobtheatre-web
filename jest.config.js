a3ac989f5dcea99b9b396672ac5425b98890bbcc
Alex Toff <me@alextoff.uk>
2020-11-15 00:16:29 +0000

    ci: Amend coverage locations and GitHub Action

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   setupFilesAfterEnv: ['<rootDir>tests/unit/setup.js'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/tests/',
+    'src/main.js',
+    'src/registerServiceWorker.js',
+    'src/extensions.js',
+    'src/server.js',
+  ],
+  collectCoverageFrom: ['src/**/*.{js,vue}'],
 };
