3903148624504aab92acd31ea1fad5dcc14d8215
streth11 <tom.streuli@gmail.com>
2020-11-24 11:24:25 +0000

    merge

--- tests/unit/components/views/production/Production.spec.js
+++ tests/unit/components/views/production/Production.spec.js
@@ -75,38 +75,4 @@ describe('Production', function () {
     // TODO: Implement after 404 ready
   });
 
-  describe('ProductionHeader', () => {
-    let productionHeaderContainer;
-
-    beforeEach(() => {
-      productionHeaderContainer = productionComponent.findComponent({ ref: 'header'});
-    });
-
-    it('fall back while producion is loading', () => {
-      expect(productionHeaderContainer.text()).to.contain(
-        'Loading Production...'
-      );
-    });
-
-    it('shows production with all details in header', async () => {
-      seedProductions();
-
-      productionComponent = mountWithRouterMock(Producion);
-      productionHeaderContainer = productionComponent.findComponent({ ref: 'header'});
-      await waitFor(() => productionComponent.vm.production);
-
-    });
-  });
-
-  let seedProductions = () => {
-    // Seed a production that can be featured
-    server.create('production', {
-      name: 'Upside Down Cake',
-      cover_image: 'http://pathto.example/my-image.png',
-      society: server.create('society', { name: 'Joe Bloggs Productions' }),
-      start_date: new Date('2020-11-14'),
-      end_date: new Date('2020-11-18'),
-      performances: server.createList('performance', 3),
-    });
-  };
 });
