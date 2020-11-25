0703d65fe29d51ab86c9576ad3a8565af183aa43
streth11 <tom.streuli@gmail.com>
2020-11-25 13:09:36 +0000

    fix merge

--- tests/unit/components/views/production/ProductionHeader.spec.js
+++ tests/unit/components/views/production/ProductionHeader.spec.js
@@ -12,26 +12,27 @@ describe('ProductionHeader', function () {
   beforeEach(() => {
     server = makeServer({ environment: 'test' });
 
-    server.create('production', {
+    let production = server.create('production', {
       name: 'Legally Ginger',
       slug: 'legally-ginger',
       cover_image: 'http://pathto.example/my-image.png',
       society: server.create('society', {
         name: 'Joe Bloggs Productions',
-        logo_image: 'http://pathto.example/my-image.png',
+        logo_image: 'http://pathto.example/logo-image.png',
       }),
       start_date: new Date('2020-11-14'),
       end_date: new Date('2020-11-18'),
-      performances: server.createList('performance', 3),
+      performances: server.create('performance', {
+        start: Date('2020-11-14'),
+        venue: server.create('venue', {
+          name: 'The New Vic',
+        }),
+      }),
     });
 
     headerContainer = mount(ProductionHeader, {
-      mocks: {
-        $route: {
-          params: {
-            productionSlug: 'legally-ginger',
-          },
-        },
+      propsData: {
+        production: production,
       },
     });
   });
@@ -40,7 +41,16 @@ describe('ProductionHeader', function () {
     server.shutdown();
   });
 
-  it('fall back while producion is loading', () => {
+  it('fall back while production is loading', async () => {
+    await headerContainer.setProps({ production: null });
     expect(headerContainer.text()).to.contain('Loading Production...');
+  });
+
+  it('correct production by correct society', () => {
+    expect(headerContainer.text()).to.contain('Legally Ginger');
+
+    expect(headerContainer.text()).to.contain('by Joe Bloggs Productions');
+
+    expect(headerContainer.text()).to.contain('The New Vic');
   });
 });
