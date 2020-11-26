68bb0348e2fbe37085dea1aa84a251e5cc255a86
streth11 <tom.streuli@gmail.com>
2020-11-26 18:10:52 +0000

    tests

--- tests/unit/components/views/production/ProductionHeader.spec.js
+++ tests/unit/components/views/production/ProductionHeader.spec.js
@@ -4,6 +4,7 @@ import ProductionHeader from '@/views/production/ProductionHeader.vue';
 
 import { mount } from '@vue/test-utils';
 import { productionService } from '@/services';
+import { fixTextSpacing } from '../../../helpers.js';
 
 describe('ProductionHeader', function () {
   let headerContainer;
@@ -12,9 +13,9 @@ describe('ProductionHeader', function () {
   beforeEach(async () => {
     server = makeServer({ environment: 'test' });
 
-    let venue = server.create('venue', {
-      name: 'The New Vic',
-    });
+    // let venue = server.create('venue', {
+    //   name: 'The New Vic',
+    // });
 
     server.create('production', {
       name: 'Legally Ginger',
@@ -26,10 +27,27 @@ describe('ProductionHeader', function () {
       }),
       start_date: new Date('2020-11-14'),
       end_date: new Date('2020-11-18'),
-      performances: server.createList('performance', 2, {
-        start: Date('2020-11-14'),
-        venue: venue,
-      }),
+      min_ticket_price: '4.34',
+      performances: [
+        server.create('performance', {
+          start: Date('2020-11-14'),
+          venue: server.create('venue', {
+            name: 'The New Vic',
+          }),
+          is_inperson: true,
+          is_online: false,
+          duration_mins: 102,
+        }),
+        server.create('performance', {
+          start: Date('2020-11-15'),
+          venue: server.create('venue', {
+            name: 'The Newer Vic',
+          }),
+          is_inperson: true,
+          is_online: false,
+          duration_mins: 112,
+        }),
+      ],
     });
 
     return productionService
@@ -52,11 +70,31 @@ describe('ProductionHeader', function () {
     expect(headerContainer.text()).to.contain('Loading Production...');
   });
 
-  it('correct production by correct society', () => {
+  it('Testing text elecemts', () => {
+    // test correct show title
     expect(headerContainer.text()).to.contain('Legally Ginger');
 
+    // test correct society perfomring show
     expect(headerContainer.text()).to.contain('by Joe Bloggs Productions');
 
-    expect(headerContainer.text()).to.contain('The New Vic');
+    // test combine of two venues
+    expect(fixTextSpacing(headerContainer.text())).to.contain(
+      'The New Vic and The Newer Vic'
+    );
+
+    // test production start and end dates
+    expect(headerContainer.text()).to.contain('14 Nov - 18 Nov 2020');
+
+    // test for performance time to be the minimum length, in nice format
+    expect(headerContainer.text()).to.contain('1 hour, 42 minutes');
+
+    // test for correct ticket price
+    expect(fixTextSpacing(headerContainer.text())).to.contain(
+      'Tickets avaliable from £4.34'
+    );
   });
+
+  it('Testing text for online only production', () => {});
+
+  it('Testing text for online and in person production', () => {});
 });
