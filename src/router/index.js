6629ccc1b873a966ec0947fba5d8787a51b30fb8
Alex Toff <me@alextoff.uk>
2020-11-28 13:37:32 +0000

    Lazy load production page and remove about page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Meta from 'vue-meta';
 import Home from '../views/Home.vue';
-import Production from '../views/production/Production.vue';
+const Production = () => import('../views/production/Production.vue');
 
 Vue.use(VueRouter);
 Vue.use(Meta);
@@ -13,16 +13,6 @@ const routes = [
     name: 'home',
     component: Home,
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-
   {
     path: '/production/:productionSlug',
     name: 'production',
