d70c3989ffb288dccea19e4b3c50cf28968be707
streth11 <tom.streuli@gmail.com>
2020-12-05 21:46:12 +0000

    login error messages

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,19 +1,8 @@
 <template>
-  <div class="bg-sta-gray h-screen login-background">
-    <div class="flex h-full justify-center items-center">
-      <user-auth-box class="shadow-2xl" />
-    </div>
+  <div>
+    <user-auth-box />
   </div>
 </template>
-
-<style scoped lang="scss">
-.login-background {
-  background-image: url('~@/assets/images/pros-arch-lit.jpg');
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: top center;
-}
-</style>
 
 <script>
 import UserAuthBox from './UserAuthBox.vue';
@@ -21,5 +10,8 @@ import UserAuthBox from './UserAuthBox.vue';
 export default {
   components: { UserAuthBox },
   name: 'login',
+  data() {
+    return {};
+  },
 };
 </script>
