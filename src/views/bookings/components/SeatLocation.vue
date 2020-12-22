4f727330118920e9fdcaa17ca4253b4b97886084
streth11 <tom.streuli@gmail.com>
2020-12-22 17:50:35 +0000

    seat location and seat type

---
+++ src/views/bookings/components/SeatLocation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div
+      class="pl-2 text-white"
+      :class="[expanded ? 'bg-sta-orange' : 'bg-sta-green']"
+      @click="$emit('select-location', true)"
+      @keyup="$emit('select-location', true)"
+    >
+      <h1 class="text-h2">{{ location.name }}</h1>
+      <div v-if="expanded" class="p-2">
+        <p>Description</p>
+      </div>
+    </div>
+    <div v-if="expanded">
+      <ticket-type
+        v-for="(ticket_type, index) in location.ticket_types"
+        :key="index"
+        :ticket_type="ticket_type"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TicketType from '@/views/bookings/components/TicketType.vue';
+
+export default {
+  name: 'seat-location',
+  components: { TicketType },
+  props: {
+    expanded: {
+      required: true,
+    },
+    location: {
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/bookings/components/TicketType.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="text-white bg-sta-gray-dark">
+    <div class="flex items-center p-2">
+      <div class="w-3/4 pl-2">{{ ticket_type.name }}</div>
+      <div class="flex-col w-1/4 space-y-1">
+        <div class="flex justify-center font-semibold font">
+          £{{ ticket_type.price_pounds }}
+        </div>
+        <div class="flex justify-center space-x-1">
+          <button
+            class="w-8 h-8 p-0 btn btn-orange"
+            @click="minusTicket"
+            @keyup="minusTicket"
+            :class="[!num_tickets ? 'btn-gray-light' : 'btn-orange']"
+            :disabled="!num_tickets"
+          >
+            -
+          </button>
+          <div
+            class="flex items-center justify-center w-8 h-8 text-black bg-white rounded-sm"
+          >
+            {{ num_tickets }}
+          </div>
+          <button
+            class="w-8 h-8 p-0 btn btn-orange"
+            @click="addTicket"
+            @keyup="addTicket"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ticket-type',
+  props: {
+    ticket_type: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      num_tickets: 0,
+    };
+  },
+  methods: {
+    addTicket() {
+      ++this.num_tickets;
+    },
+    minusTicket() {
+      --this.num_tickets;
+    },
+  },
+};
+</script>
