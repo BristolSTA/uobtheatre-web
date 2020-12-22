b055a696150207f129f03ed257065d23bd43753b
streth11 <tom.streuli@gmail.com>
2020-12-22 17:51:07 +0000

    ticket selection

--- src/views/bookings/stages/TicketSelectionStage.vue
+++ src/views/bookings/stages/TicketSelectionStage.vue
@@ -1,15 +1,59 @@
 <template>
   <div class="text-white">
-    <h2 class="text-h2">How many in your party?</h2>
+    <seat-location
+      v-for="(location, locationIndex) in locations"
+      :key="locationIndex"
+      :location="location"
+      :expanded="true"
+    />
   </div>
 </template>
 
 <script>
+import SeatLocation from '@/views/bookings/components/SeatLocation.vue';
+
 export default {
+  name: 'ticket-selection-stage',
+  components: { SeatLocation },
   props: {
     production: {
       required: true,
     },
   },
+  data() {
+    return {
+      expanded: true,
+      locations: [
+        {
+          name: 'Standing',
+          ticket_types: [
+            {
+              name: 'Adult',
+              price_pounds: '12.00',
+            },
+            {
+              name: 'Child',
+              price_pounds: '5.00',
+            },
+          ],
+        },
+        {
+          name: 'Seated',
+          ticket_types: [
+            {
+              name: 'Adult',
+              price_pounds: '20.00',
+            },
+            {
+              name: 'Child',
+              price_pounds: '10.00',
+            },
+          ],
+        },
+      ],
+      selected_location_index: null,
+    };
+  },
+  methods: {},
 };
 </script>
