52b3334d9f9ec762424a6ed525bc8884933a4405
streth11 <tom.streuli@gmail.com>
2021-01-09 17:45:48 +0000

    user overview

---
+++ src/components/overview/UserOverview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <overview-box :subtitle="true">
+      <template v-slot:title>Your Details</template>
+      <template v-slot:subtitle> Joe Bloogs </template>
+      <div>joe.bloggs@example.org</div>
+    </overview-box>
+  </div>
+</template>
+
+<script>
+import OverviewBox from '@/components/overview/OverviewBox.vue';
+
+export default {
+  name: 'user-overview',
+  components: { OverviewBox },
+  props: {
+    booking: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+};
+</script>
--- src/views/booking/stages/OverviewStage.vue
+++ src/views/booking/stages/OverviewStage.vue
@@ -1,36 +1,25 @@
 <template>
-  <div>
-    <div class="px-4 py-2 mb-2 text-white md:text-center bg-sta-gray-light">
-      <p class="text-h3">{{ production.name }}</p>
-      <p class="text-sta-orange">
-        {{ production.society.name }}
-      </p>
-    </div>
+  <div class="space-y-2">
+    <performance-overview-box
+      :production="production"
+      :performance="booking.performance"
+    />
     <div class="grid gap-2 md:grid-cols-2 justify-evenly">
-      <performance-overview :performance="booking.performance" />
-      <div
-        v-if="production.warnings"
-        class="p-3 border bg-sta-gray text-sta-rouge border-sta-rouge"
-        ref="warnings"
-      >
-        <span class="text-xl font-semibold uppercase">Audience Warnings</span>
-        <ul class="list-disc list-inside">
-          <li v-for="(warning, index) in production.warnings" :key="index">
-            {{ warning }}
-          </li>
-        </ul>
-      </div>
+      <venue-overview :venue_slug="booking.performance.venue.slug" />
+      <user-overview :booking="booking" />
     </div>
   </div>
 </template>
 
 <script>
 import Booking from '@/classes/Booking';
-import PerformanceOverview from '@/components/production/PerformanceOverview.vue';
+import PerformanceOverviewBox from '@/components/overview/PerformanceOverviewBox.vue';
+import UserOverview from '@/components/overview/UserOverview.vue';
+import VenueOverview from '@/components/overview/VenueOverview.vue';
 
 export default {
   name: 'overview-stage',
-  components: { PerformanceOverview },
+  components: { VenueOverview, PerformanceOverviewBox, UserOverview },
   props: {
     production: {
       required: true,
