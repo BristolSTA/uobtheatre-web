9cff51ac041624534931c9c34f2464eda5f0b02f
streth11 <tom.streuli@gmail.com>
2021-03-04 23:53:08 +0000

    tickets

---
+++ src/components/booking/Ticket.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="p-4 pt-2 text-black bg-white w-80 rounded-xl">
+    <h1 class="text-h2">TRASh</h1>
+    <p>
+      Date: {{ booking.performance.start | dateFormat('EEEE d MMMM kkkk') }}
+    </p>
+    <p>Doors: {{ booking.performance.doorsOpen | dateFormat('T') }}</p>
+    <p>Start: {{ booking.performance.start | dateFormat('T') }}</p>
+    <div class="flex justify-between w-full font-semibold">
+      <p>1x {{ ticket.concession_type.name }}</p>
+      <p class="text-right">{{ ticket.seat_group.name }}</p>
+    </div>
+    <div class="flex justify-center w-full py-2">
+      <qrcode-vue :value="qrString" level="L" size="240" />
+    </div>
+    <p>Booking Ref: {{ booking.bookingReference }}</p>
+    <!-- <p>Paid On: 3 Jan 2020</p> -->
+    <p>By: {{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+import lo from 'lodash';
+import QrcodeVue from 'qrcode.vue';
+
+import Booking from '@/classes/Booking';
+
+export default {
+  name: 'Ticket',
+  components: {
+    QrcodeVue,
+  },
+  props: {
+    booking: {
+      required: true,
+      type: Booking,
+    },
+    ticket: {
+      required: true,
+      type: Object,
+    },
+    user: {
+      required: true,
+      type: Object,
+    },
+  },
+  computed: {
+    qrString() {
+      return lo.join(
+        [
+          `ref:${this.booking.bookingReference}`,
+          `concessionType:${this.ticket.concession_type.name}`,
+          `seatGroup:${this.ticket.seat_group.name}`,
+          `user:Jelgar`,
+        ],
+        ';'
+      );
+    },
+    fullName() {
+      return lo.join([this.user.firstName, this.user.lastName], ' ');
+    },
+  },
+};
+</script>
--- src/views/user/ViewBooking.vue
+++ src/views/user/ViewBooking.vue
@@ -24,6 +24,36 @@
         <payment-overview class="lg:col-span-1" :booking="booking" />
         <tickets-overview class="lg:col-span-2" :booking="booking" />
       </div>
+
+      <div class="mt-4">
+        <div
+          ref="header"
+          class="flex py-2 pl-4 cursor-pointer hover:bg-opacity-80"
+          :class="[expanded ? 'bg-sta-orange' : 'bg-sta-green']"
+          @click="ticketToggle"
+          @keypress="ticketToggle"
+        >
+          <div class="flex-grow">
+            <h3 class="inline-block text-h3 lg:text-h2">Tickets</h3>
+          </div>
+          <div class="flex items-center pr-4 text-3xl">
+            <font-awesome-icon
+              :icon="expanded ? 'chevron-up' : 'chevron-down'"
+            />
+          </div>
+        </div>
+        <div v-if="expanded" class="p-4 bg-sta-gray-dark">
+          <div class="flex justify-center w-full space-x-4">
+            <ticket
+              v-for="(ticket, index) in booking.tickets"
+              :key="index"
+              :booking="booking"
+              :ticket="ticket"
+              :user="user"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +64,7 @@ import PaymentOverview from '@/components/booking/overview/PaymentOverview.vue';
 import PerformanceOverview from '@/components/booking/overview/PerformanceOverview.vue';
 import TicketsOverview from '@/components/booking/overview/TicketsOverview.vue';
 import VenueOverview from '@/components/booking/overview/VenueOverview.vue';
+import Ticket from '@/components/booking/Ticket.vue';
 import ProductionBanner from '@/components/production/ProductionBanner.vue';
 
 export default {
@@ -44,12 +75,15 @@ export default {
     TicketsOverview,
     ProductionBanner,
     PaymentOverview,
+    Ticket,
   },
   props: {},
   data() {
     return {
       booking: new Booking(),
       production: null,
+      user: null,
+      expanded: false,
     };
   },
   apollo: {
@@ -59,9 +93,23 @@ export default {
       //   bookingId: 1,
       // },
       result({ data }) {
+        this.user = (({ firstName, lastName, email }) => ({
+          firstName,
+          lastName,
+          email,
+        }))(data.authUser);
         this.booking.updateFromAPIData(data.authUser.bookings.edges[0].node);
         this.production = this.booking.performance.production;
       },
+    },
+  },
+  methods: {
+    ticketToggle() {
+      if (this.expanded) {
+        this.expanded = false;
+      } else {
+        this.expanded = true;
+      }
     },
   },
 };
