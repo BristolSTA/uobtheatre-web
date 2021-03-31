7a82543bd0e0472199c9b3b88fb038dcc138312c
Alex Toff <me@alextoff.uk>
2021-03-31 18:46:48 +0100

    Fix large build sizes

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
     '@nuxtjs/eslint-module',
 
     // // https://go.nuxtjs.dev/stylelint
-    // '@nuxtjs/stylelint-module',
+    '@nuxtjs/stylelint-module',
 
     // TailwindCSS
     '@nuxtjs/tailwindcss',
@@ -103,9 +103,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    terser: false,
-  },
+  build: {},
 
   // Apollo Configuration
   apollo: {
