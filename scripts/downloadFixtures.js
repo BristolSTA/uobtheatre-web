e3af23025c12c530cd7aca106caf5112b3c485e1
tom <tom.streuli@gmail.com>
2021-05-10 16:13:20 +0000

    database update

--- scripts/downloadFixtures.js
+++ scripts/downloadFixtures.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Fetches the GraphQL API Test Fixtures from the uobtheatre-api repository, and installs it into the fake API
  *
@@ -7,41 +8,41 @@
  * yarn api:download-fixtures update <commit> [ Downloads the API Test Fixtures for a specific commit, and locks to that commit ]
  */
 return (async () => {
-  const got = require('got');
-  const fs = require('fs');
-  let url = 'https://raw.githubusercontent.com/BristolSTA/uobtheatre-api/';
-  let lockFile = './fixtures.lock';
+  const got = require('got')
+  const fs = require('fs')
+  let url = 'https://raw.githubusercontent.com/BristolSTA/uobtheatre-api/'
+  const lockFile = './fixtures.lock'
 
-  if (process.argv[2] == 'update') {
-    let lock = process.argv[3] ? process.argv[3] : null;
+  if (process.argv[2] === 'update') {
+    let lock = process.argv[3] ? process.argv[3] : null
     if (!lock) {
       const response = await got(
         'https://api.github.com/repos/BristolSTA/uobtheatre-api/commits/main'
-      ).json();
-      lock = response.sha;
+      ).json()
+      lock = response.sha
     }
-    url += lock;
+    url += lock
 
     // Update lock file
-    fs.writeFile(lockFile, lock, () => {});
-    console.log(`Lock updated, now tracking commit ${lock}`);
+    fs.writeFile(lockFile, lock, () => {})
+    console.log(`Lock updated, now tracking commit ${lock}`)
   } else {
     if (!fs.existsSync(lockFile)) {
       console.log(
-        '\x1b[41m%s\x1b[0m',
+        '\x1B[41m%s\x1B[0m',
         'No FixtureSet Lock File Exists. Run with "update"'
-      );
-      return;
+      )
+      return
     }
 
     // Load from lock
-    let data = fs.readFileSync(lockFile, 'utf8');
-    url += data;
-    console.log(`Using fixtureSet from commit ${data}`);
+    const data = fs.readFileSync(lockFile, 'utf8')
+    url += data
+    console.log(`Using fixtureSet from commit ${data}`)
   }
 
-  const response = await got(url + '/db.json');
-  fs.writeFile('src/fakeApi/db.json', response.body, () => {});
+  const response = await got(url + '/db.json')
+  fs.writeFile('fakeApi/db.json', response.body, () => {})
 
-  console.log('\x1b[32m%s\x1b[0m', 'Real API Test FixtureSet Downloaded');
-})();
+  console.log('\x1B[32m%s\x1B[0m', 'Real API Test FixtureSet Downloaded')
+})()
