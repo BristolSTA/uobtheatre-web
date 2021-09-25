c5622fba94e10af3ae78ecfd6b9785d6aa8d331c
tom <tom.streuli@gmail.com>
2021-09-25 16:55:22 +0000

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,12 +55,20 @@ module.exports = {
       minWidth: {
         '1/2': '50%',
       },
+      minHeight: {
+        '1/4': '25vh',
+        '2/5': '40vh',
+      },
+      dropShadow: {
+        dark: '0 2px 4px rgba(0, 0, 0, 0.35)',
+      },
     },
   },
   variants: {
     extend: {
       backgroundColor: ['checked', 'odd', 'even'],
       flexDirection: ['odd'],
+      dropShadow: ['hover'],
     },
   },
   plugins: [require('@tailwindcss/forms')],
