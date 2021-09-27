c7b7f054964c618ca09aa2876a8ecff077111b1c
tom <tom.streuli@gmail.com>
2021-09-27 19:01:00 +0000

    T&C's + privacy policy

--- pages/terms.vue
+++ pages/terms.vue
@@ -1,10 +1,91 @@
 <template>
-  <div class="container mx-auto">
-    <h1>Terms and Conditions</h1>
-    <p>To be written 😇</p>
+  <div class="container mx-auto text-page">
+    <h1 class="my-4 text-h1">Terms and Conditions</h1>
+    <p>
+      This Policy applies as an agreement between you, the User of this Web Site
+      and University of Bristol Students' Union Ltd. (Stage Technicians'
+      Association) the owner and provider of this Web Site. This Policy applies
+      to our use of any and all Data collected by us in relation to your use of
+      the Web Site and any Services or Systems therein.
+    </p>
+    <p>
+      Use of this Web Site is subject to agreement with the following terms and
+      conditions:
+    </p>
+    <h2>1. Purchase of Tickets</h2>
+    <p>
+      Tickets are supplied electronically in the form of a Booking Code which
+      must be shown at the Box Office or entrance to gain entry to the theatre.
+      Your Booking Code is emailed to you when you purchase a ticket, and can
+      also be seen on your "My Account" page
+    </p>
+    <p>
+      All tickets are for General Admission and specific seats may not be
+      reserved.
+    </p>
+    <p>
+      Tickets purchased online are subject to a booking fee. Check the total
+      amount before proceeding to the payment pages. Some shows also include a
+      Theatre Improvement Levy which provides a valuable source of funding to
+      theatre maintenance and improvements at Bristol SU.
+    </p>
+    <h2>2. Problems with Booking</h2>
+    <p>
+      In the unlikely event that you have paid but not received your Booking
+      Code, please inform the Site Administrators on support@uobtheatre.com. Use
+      this address for any queries or issues relating to the Web Site and its
+      component services.
+    </p>
+    <h2>3. Refund Policy</h2>
+    <p>
+      We cannot exchange, refund, transfer or return tickets unless the event in
+      question is cancelled or details of the event are significantly changed
+      (for example, show time, venue, date etc).
+    </p>
+    <p>
+      On occasions where an event is officially cancelled we'll contact you via
+      the email address you gave us when registering. We'll then offer you the
+      choice of a refund or, where available, tickets to the rescheduled event.
+    </p>
+    <h2>4. Right of Admission</h2>
+    <p>
+      Right of admission to any Bristol Students' Union venue is at the
+      discretion of the Front-of-House Manager. The Front-of-House Manager has
+      the right to eject persons from the venue who are acting in a disruptive,
+      overly intoxicated or threatening manner. A refund will not be made in
+      such a situation.
+    </p>
+    <p>
+      Right of admission to any non-Bristol Students' Union venue is at the
+      discretion of the venue manager there.
+    </p>
+    <h2>5. Customer Obligation</h2>
+    <p>
+      Bristol Student Theatre is a vendor of tickets for amateur performances in
+      venues staffed by volunteers. Mistakes can be made on occasion.
+      Information is believed to be correct at time of publication. Your
+      patience is appreciated. Thank you!
+    </p>
+    <p>
+      These Terms and Conditions do not affect your statutory rights as a
+      consumer under English law.
+    </p>
   </div>
 </template>
 
 <script>
+/* eslint-disable vue/no-v-html */
 export default {}
 </script>
+
+<style lang="scss">
+.text-page {
+  h2 {
+    @apply text-h2 mt-2 font-semibold;
+  }
+
+  p {
+    @apply mb-4 md:text-justify;
+  }
+}
+</style>
