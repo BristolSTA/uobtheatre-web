61ef50210b10ec7b18fed98a1d5cab4a3951e24e
Alex Toff <me@alextoff.uk>
2021-10-06 21:38:42 +0000

    add some text

--- pages/box-office/_performanceId/sell/pay.vue
+++ pages/box-office/_performanceId/sell/pay.vue
@@ -15,7 +15,7 @@
           <h3 class="text-h3 text-sta-rouge">Complete User Details First</h3>
           <p>
             We collect the booker's details to send them their receipt and to
-            help process refunds.
+            help process refunds. We never send unsolicited emails.
           </p>
         </div>
         <template v-else>
@@ -28,14 +28,7 @@
               <template v-if="booking.totalPrice > 0">
                 <button
                   v-if="enabledMethods.squarePOS && availableTerminals.length"
-                  class="
-                    p-2
-                    transition-colors
-                    rounded
-                    bg-sta-green
-                    hover:bg-sta-green-dark
-                    focus:outline-none
-                  "
+                  class="p-2 transition-colors rounded  bg-sta-green hover:bg-sta-green-dark focus:outline-none"
                   @click="terminalDevice ? pay('SQUARE_POS') : selectTerminal()"
                 >
                   <font-awesome-icon icon="money-bill" />
@@ -44,16 +37,7 @@
                 </button>
                 <button
                   v-else-if="enabledMethods.manualCard"
-                  class="
-                    p-2
-                    transition-colors
-                    rounded
-                    bg-sta-green
-                    hover:bg-sta-green-dark
-                    focus:outline-none
-                    btn
-                    disabled
-                  "
+                  class="p-2 transition-colors rounded  bg-sta-green hover:bg-sta-green-dark focus:outline-none btn disabled"
                   @click="paymentMode = 'CARD'"
                 >
                   <font-awesome-icon icon="money-check-alt" />
@@ -61,14 +45,7 @@
                 </button>
                 <button
                   v-if="enabledMethods.cash"
-                  class="
-                    p-2
-                    transition-colors
-                    rounded
-                    bg-sta-green
-                    hover:bg-sta-green-dark
-                    focus:outline-none
-                  "
+                  class="p-2 transition-colors rounded  bg-sta-green hover:bg-sta-green-dark focus:outline-none"
                   @click="paymentMode = 'CASH'"
                 >
                   <font-awesome-icon icon="money-bill" />
@@ -77,14 +54,7 @@
               </template>
               <button
                 v-else
-                class="
-                  p-2
-                  transition-colors
-                  rounded
-                  bg-sta-green
-                  hover:bg-sta-green-dark
-                  focus:outline-none
-                "
+                class="p-2 transition-colors rounded  bg-sta-green hover:bg-sta-green-dark focus:outline-none"
                 @click="pay(null)"
               >
                 <font-awesome-icon icon="money-bill" />
@@ -114,13 +84,7 @@
             </div>
             <div v-if="paymentMode" class="mt-4 text-center">
               <button
-                class="
-                  p-2
-                  rounded
-                  animate-pulse
-                  bg-sta-orange
-                  hover:bg-sta-orange-dark
-                "
+                class="p-2 rounded  animate-pulse bg-sta-orange hover:bg-sta-orange-dark"
                 @click="pay(paymentMode)"
               >
                 <strong
@@ -131,14 +95,7 @@
             <template v-if="paymentMode == 'SQUARE_POS'" #overlay>
               <div>
                 <button
-                  class="
-                    p-2
-                    mt-4
-                    transition-colors
-                    rounded
-                    bg-sta-rouge
-                    hover:bg-sta-rouge-dark
-                  "
+                  class="p-2 mt-4 transition-colors rounded  bg-sta-rouge hover:bg-sta-rouge-dark"
                   @click="cancelSquarePOSPayment"
                 >
                   Cancel
@@ -153,26 +110,26 @@
 </template>
 
 <script>
-import TextInput from '@/components/ui/TextInput.vue'
-import Booking from '@/classes/Booking'
-import BookingPriceOverview from '@/components/booking/overview/BookingPriceOverview.vue'
-import TicketsOverview from '@/components/booking/overview/TicketsOverview.vue'
-import PayBooking from '@/graphql/mutations/booking/PayBooking.gql'
-import SetBookingUser from '@/graphql/mutations/booking/SetBookingUser.gql'
+import TextInput from '@/components/ui/TextInput.vue';
+import Booking from '@/classes/Booking';
+import BookingPriceOverview from '@/components/booking/overview/BookingPriceOverview.vue';
+import TicketsOverview from '@/components/booking/overview/TicketsOverview.vue';
+import PayBooking from '@/graphql/mutations/booking/PayBooking.gql';
+import SetBookingUser from '@/graphql/mutations/booking/SetBookingUser.gql';
 import {
   getValidationErrors,
   performMutation,
   silentErrorHandler,
   swal,
-} from '@/utils'
-import AllErrorsDisplay from '@/components/ui/AllErrorsDisplay.vue'
-import LoadingContainer from '@/components/ui/LoadingContainer.vue'
+} from '@/utils';
+import AllErrorsDisplay from '@/components/ui/AllErrorsDisplay.vue';
+import LoadingContainer from '@/components/ui/LoadingContainer.vue';
 
 const enabledMethods = {
   cash: true,
   manualCard: false,
   squarePOS: true,
-}
+};
 
 export default {
   components: {
@@ -207,27 +164,27 @@ export default {
         { id: 2, deviceId: '345', name: 'Terminal 2' },
       ],
       enabledMethods,
-    }
+    };
   },
   computed: {
     canPay() {
-      return /\S+@\S+\.\S+/.test(this.user.email)
+      return /\S+@\S+\.\S+/.test(this.user.email);
     },
     cashChange() {
       if (!this.tendered || this.tendered < this.booking.totalPricePounds)
-        return ''
-      return (this.tendered - this.booking.totalPricePounds).toFixed(2)
+        return '';
+      return (this.tendered - this.booking.totalPricePounds).toFixed(2);
     },
     terminalDevice() {
-      return this.$store.state['box-office'].terminalDevice
+      return this.$store.state['box-office'].terminalDevice;
     },
   },
   async mounted() {
-    if (!this.booking.tickets.length) return this.$router.replace('./')
+    if (!this.booking.tickets.length) return this.$router.replace('./');
     try {
       this.availableTerminals = await this.$store.dispatch(
         'box-office/retrieveAvailableTerminalDevices'
-      )
+      );
       // If we already have a device, let's check it's still good
       if (
         this.terminalDevice &&
@@ -235,10 +192,10 @@ export default {
           (device) => device.id === this.terminalDevice.id
         )
       ) {
-        this.$store.commit('box-office/SET_TERMINAL_DEVICE', null)
+        this.$store.commit('box-office/SET_TERMINAL_DEVICE', null);
       }
     } catch (e) {
-      silentErrorHandler(e)
+      silentErrorHandler(e);
     }
   },
   apollo: {
@@ -248,10 +205,10 @@ export default {
         return {
           bookingId: this.booking.id,
           performanceId: this.booking.performance.id,
-        }
+        };
       },
       skip() {
-        return !this.paying || this.paymentMode !== 'SQUARE_POS'
+        return !this.paying || this.paymentMode !== 'SQUARE_POS';
       },
       update: (data) =>
         data.performance.bookings.edges.length
@@ -259,7 +216,7 @@ export default {
           : null,
       result() {
         if (this.refreshedBooking.status.value === 'PAID') {
-          this.bookingCompleted(this.refreshedBooking)
+          this.bookingCompleted(this.refreshedBooking);
         }
       },
       pollInterval: 1000,
@@ -267,28 +224,28 @@ export default {
   },
   methods: {
     cancelSquarePOSPayment() {
-      this.paying = false
-      this.paymentMode = null
+      this.paying = false;
+      this.paymentMode = null;
     },
     async selectTerminal() {
       const terminalOptions = Object.fromEntries(
         this.availableTerminals.map((device, index) => [index, device.name])
-      )
+      );
       const { value } = await swal.fire({
         title: 'Select a payment device',
         input: 'select',
         inputOptions: terminalOptions,
         showCancelButton: true,
-      })
-      const device = value !== null ? this.availableTerminals[value] : null
+      });
+      const device = value !== null ? this.availableTerminals[value] : null;
       if (device) {
-        this.$store.commit('box-office/SET_TERMINAL_DEVICE', device)
+        this.$store.commit('box-office/SET_TERMINAL_DEVICE', device);
       }
     },
     async pay(method) {
-      if (this.paying) return
-      this.paying = true
-      this.paymentMode = method
+      if (this.paying) return;
+      this.paying = true;
+      this.paymentMode = method;
 
       try {
         // Set the booking's user
@@ -302,7 +259,7 @@ export default {
             },
           },
           'updateBooking'
-        )
+        );
 
         const data = await performMutation(
           this.$apollo,
@@ -317,26 +274,26 @@ export default {
             },
           },
           'payBooking'
-        )
+        );
 
         if (method === 'SQUARE_POS') {
-          return
+          return;
         }
 
-        return this.bookingCompleted(data.payBooking.booking)
+        return this.bookingCompleted(data.payBooking.booking);
       } catch (e) {
-        this.errors = getValidationErrors(e)
-        this.booking.refreshIdempotencyKey()
-        this.paying = false
+        this.errors = getValidationErrors(e);
+        this.booking.refreshIdempotencyKey();
+        this.paying = false;
       }
     },
     bookingCompleted(bookingData) {
-      this.paying = false
-      this.booking.updateFromAPIData(bookingData)
+      this.paying = false;
+      this.booking.updateFromAPIData(bookingData);
       this.$router.push(
         `/box-office/${this.booking.performance.id}/sell/complete`
-      )
+      );
     },
   },
-}
+};
 </script>
