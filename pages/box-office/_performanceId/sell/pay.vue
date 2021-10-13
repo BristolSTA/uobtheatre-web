747e51fdc618225beb5e4350af5a470db0654591
Alex Toff <me@alextoff.uk>
2021-10-13 22:22:28 +0000

    style: Box office POS fixes

--- pages/box-office/_performanceId/sell/pay.vue
+++ pages/box-office/_performanceId/sell/pay.vue
@@ -5,11 +5,11 @@
       <booking-price-overview :booking="booking" />
     </div>
     <div class="grid gap-2 my-4 md:grid-cols-2">
-      <div class="p-3 bg-sta-gray-dark rounded">
+      <div class="p-3 rounded bg-sta-gray-dark">
         <h2 class="text-center text-h2">Details</h2>
         <text-input v-model="user.email" name="Email" type="email" required />
       </div>
-      <div class="p-3 bg-sta-gray-dark rounded">
+      <div class="p-3 rounded bg-sta-gray-dark">
         <h2 class="text-center text-h2">Payment</h2>
         <div v-if="!canPay" class="text-center">
           <h3 class="text-sta-rouge text-h3">Complete User Details First</h3>
@@ -24,19 +24,18 @@
           </h3>
           <loading-container :loading="paying">
             <all-errors-display class="text-center" :errors="errors" />
-            <div class="grid gap-2 grid-cols-2 text-center">
+            <div class="grid grid-cols-2 gap-2 text-center">
               <template v-if="booking.totalPrice > 0">
                 <button
                   v-if="enabledMethods.squarePOS && availableTerminals.length"
                   class="
-                    btn
-                    disabled
                     p-2
+                    transition-colors
+                    rounded
+                    btn
                     bg-sta-green
                     hover:bg-sta-green-dark
-                    rounded
                     focus:outline-none
-                    transition-colors
                   "
                   @click="terminalDevice ? pay('SQUARE_POS') : selectTerminal()"
                 >
@@ -47,14 +46,13 @@
                 <button
                   v-else-if="enabledMethods.manualCard"
                   class="
-                    btn
-                    disabled
                     p-2
+                    transition-colors
+                    rounded
+                    btn
                     bg-sta-green
                     hover:bg-sta-green-dark
-                    rounded
                     focus:outline-none
-                    transition-colors
                   "
                   @click="paymentMode = 'CARD'"
                 >
@@ -65,11 +63,11 @@
                   v-if="enabledMethods.cash"
                   class="
                     p-2
+                    transition-colors
+                    rounded
                     bg-sta-green
                     hover:bg-sta-green-dark
-                    rounded
                     focus:outline-none
-                    transition-colors
                   "
                   @click="paymentMode = 'CASH'"
                 >
@@ -81,11 +79,11 @@
                 v-else
                 class="
                   p-2
+                  transition-colors
+                  rounded
                   bg-sta-green
                   hover:bg-sta-green-dark
-                  rounded
                   focus:outline-none
-                  transition-colors
                 "
                 @click="pay(null)"
               >
@@ -94,14 +92,14 @@
               </button>
             </div>
             <div v-if="paymentMode == 'CASH'" class="my-2">
-              <div class="py-2 text-center text-xl">Change Calculator</div>
-              <div class="grid gap-2 grid-cols-2">
+              <div class="py-2 text-xl text-center">Change Calculator</div>
+              <div class="grid grid-cols-2 gap-2">
                 <div class="flex items-center w-full">
                   <span class="mx-2 text-xl font-semibold">£</span>
                   <input
                     v-model.number="tendered"
                     type="text"
-                    class="p-1 w-full text-gray-800 rounded outline-none"
+                    class="w-full p-1 text-gray-800 rounded outline-none"
                     placeholder="Tendered"
                   />
                 </div>
@@ -118,9 +116,9 @@
               <button
                 class="
                   p-2
+                  rounded
                   bg-sta-orange
                   hover:bg-sta-orange-dark
-                  rounded
                   animate-pulse
                 "
                 @click="pay(paymentMode)"
@@ -134,12 +132,12 @@
               <div>
                 <button
                   class="
-                    mt-4
                     p-2
+                    mt-4
+                    transition-colors
+                    rounded
                     bg-sta-rouge
                     hover:bg-sta-rouge-dark
-                    rounded
-                    transition-colors
                   "
                   @click="cancelSquarePOSPayment"
                 >
