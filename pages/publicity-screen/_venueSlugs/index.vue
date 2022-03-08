221efbae2f15a4757a5cbc680d23ea90c3ab905f
Robbie Bradshaw <dc21518@bristol.ac.uk>
2022-03-08 14:29:56 +0000

    Auto hide the mouse
    After 2 seconds of being still the mouse becomes invisible, and it reappears when it's moved again

--- pages/publicity-screen/_venueSlugs/index.vue
+++ pages/publicity-screen/_venueSlugs/index.vue
@@ -83,6 +83,26 @@
 </template>
 
 <script>
+    var mouseTimer = null,
+        cursorVisible = true;
+
+    function disappearCursor() {
+        mouseTimer = null;
+        document.body.style.cursor = "none";
+        cursorVisible = false;
+    }
+    document.onmousemove = function() {
+        if (mouseTimer) {
+            window.clearTimeout(mouseTimer);
+        };
+        if (!cursorVisible) {
+            document.body.style.cursor = "";
+            cursorVisible = true;
+        };
+        mouseTimer = window.setTimeout(disappearCursor, 2000);
+    }; 
+</script>
+<script>
 import VenueUpcomingProductionsQuery from '@/graphql/queries/venue/VenueUpcomingProductions.gql'
 import { DateTime } from 'luxon'
 import QrcodeVue from 'qrcode.vue'
