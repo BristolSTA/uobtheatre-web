ebaa301ca7c4e5f45116647727141d88b4dc059a
Alex Toff <me@alextoff.uk>
2022-10-23 19:06:05 +0000

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    node: true,
+    browser: true
   },
   ignorePatterns: ['tests/unit/test.spec.example.js'],
-  parser: '@babel/eslint-parser',
-  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
   extends: [
+    'plugin:vue/essential',
     '@nuxtjs',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    '@nuxtjs/eslint-config-typescript'
   ],
   rules: {
-    'import/no-named-as-default': 'off',
+    'import/no-named-as-default': 'off'
   },
   overrides: [
     {
       files: ['*.test.js', '*.spec.js'],
       rules: {
-        'no-unused-expressions': 'off',
-      },
-    },
-  ],
+        'no-unused-expressions': 'off'
+      }
+    }
+  ]
 }
