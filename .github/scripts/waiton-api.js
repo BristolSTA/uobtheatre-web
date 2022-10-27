59ffa115f7958af1bbd6c31872d7b0df2da8d694
Alex Toff <me@alextoff.uk>
2022-10-27 17:11:28 +0000

    Attempt to fix CI timeout

---
+++ .github/scripts/waiton-api.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+(async function () {
+    function sleep(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    var http = require('http')
+
+    const url = process.argv[2]
+    const timeout = process.argv[3] || 60
+    const sampleTime = 1000
+
+    var continuePolling = true
+    const startTime = new Date()
+
+    while (continuePolling) {
+        var req = http.request(url, { method: 'HEAD' }, function (r) {
+            continuePolling = false;
+            process.exit(0)
+        }).on('error', err => {
+            console.log('Error: ', err.message);
+        });
+        req.end();
+
+        if ((new Date() - startTime) >= timeout * 1000) {
+            console.log("Aborting due to timeout")
+            process.exit(1)
+        }
+
+        await sleep(sampleTime)
+    }
+
+})()
