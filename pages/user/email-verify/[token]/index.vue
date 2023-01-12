eb78a857bf956b913feb3775a6f6143d93718dec
Alex Toff <me@alextoff.uk>
2023-01-12 15:03:22 +0000

    Get rid of old email-verify that didn't do what it was meant to

--- pages/user/email-verify/[token]/index.vue
+++ pages/user/email-verify/[token]/index.vue
@@ -3,15 +3,38 @@
     class="flex items-center justify-center p-6 min-h-full text-white bg-sta-gray"
   >
     <div class="relative text-center">
-      <template v-if="loading">
-        <h1 class="text-h3">Verifying email...</h1>
+      <template v-if="!addedOk && loading">
+        <h1 class="text-h3">Adding email...</h1>
         <div>
           <loading-icon size-class="text-h1" />
         </div>
       </template>
+      <template v-else-if="addedOk">
+        <h1 class="text-h3">Complete email change</h1>
+        <p>
+          To complete the change of your account's email, enter your password
+          below
+        </p>
+        <loading-container :loading="loading">
+          <form class="p-6" @submit.prevent="finishSwap">
+            <UiNonFieldError :errors="errors" />
+            <text-input
+              v-model="password"
+              name="Password"
+              type="password"
+              :errors="errors"
+              error-key="password"
+              autocomplete="current-password"
+              required
+              class="mt-4"
+            />
+            <button class="btn btn-green mt-4">Complete Change</button>
+          </form>
+        </loading-container>
+      </template>
       <template v-else>
         <font-awesome-icon class="text-sta-rouge text-h1" icon="times-circle" />
-        <h1 class="text-h3">There was an error verifying this email</h1>
+        <h1 class="text-h3">There was an error activating this email</h1>
         <p>This activation has either expired or doesn't exist!</p>
       </template>
     </div>
@@ -20,24 +43,37 @@
 <script>
 import gql from 'graphql-tag';
 
+import LoadingContainer from '@/components/ui/LoadingContainer.vue';
+
+import TextInput from '~~/components/ui/Input/UiInputText.vue';
 import LoadingIcon from '~~/components/ui/UiLoadingIcon.vue';
 
 import { getValidationErrors, performMutation } from '~~/utils/api';
 import { swalToast } from '~~/utils/alerts';
+
 import ErrorsPartial from '@/graphql/partials/ErrorsPartial';
+import useAuthStore from '@/store/auth';
+
+definePageMeta({
+  middleware: ['authed']
+});
 
 export default defineNuxtComponent({
   components: {
+    TextInput,
+    LoadingContainer,
     LoadingIcon
   },
   data() {
     return {
       password: null,
-      loading: true
+      addedOk: false,
+      loading: true,
+      errors: null
     };
   },
   head: {
-    title: 'Verify Email'
+    title: 'Change Email'
   },
   async mounted() {
     try {
@@ -46,7 +82,7 @@ export default defineNuxtComponent({
         {
           mutation: gql`
           mutation ($token: String!) {
-            verifyAccount(token: $token) {
+            verifySecondaryEmail(token: $token) {
                 ${ErrorsPartial}
             }
           }
@@ -55,18 +91,69 @@ export default defineNuxtComponent({
             token: this.$route.params.token
           }
         },
-        'verifyAccount'
+        'verifySecondaryEmail'
       );
-      swalToast.fire({
-        position: 'bottom-end',
-        icon: 'success',
-        title: 'Email verified!'
-      });
-      return useRouter().replace('/login');
+      this.addedOk = true;
     } catch (e) {
-      this.errors = getValidationErrors(e);
+      this.addedOk = false;
     }
     this.loading = false;
+  },
+  methods: {
+    async finishSwap() {
+      this.loading = true;
+      try {
+        // Swap emails
+        await performMutation(
+          this.$apollo,
+          {
+            mutation: gql`
+            mutation ($password: String!) {
+              swapEmails(password: $password) {
+                  ${ErrorsPartial}
+              }
+            }
+          `,
+            variables: {
+              password: this.password
+            }
+          },
+          'swapEmails'
+        );
+
+        // Refresh auth - the old token won't be valid as the email address has changed, and this is contained in the JWT
+        const authStore = useAuthStore();
+        await authStore.refreshUsingToken();
+
+        // Remove the old email
+        await performMutation(
+          this.$apollo,
+          {
+            mutation: gql`
+            mutation ($password: String!) {
+              removeSecondaryEmail(password: $password) {
+                  ${ErrorsPartial}
+              }
+            }
+          `,
+            variables: {
+              password: this.password
+            }
+          },
+          'removeSecondaryEmail'
+        );
+
+        swalToast.fire({
+          position: 'bottom-end',
+          icon: 'success',
+          title: 'Email changed!'
+        });
+        return useRouter().replace('/user');
+      } catch (e) {
+        this.errors = getValidationErrors(e);
+      }
+      this.loading = false;
+    }
   }
 });
 </script>
--- tests/unit/pages/user/EmailVerify.spec.js
+++ tests/unit/pages/user/EmailVerify.spec.js
@@ -6,18 +6,18 @@ import {
   GenericMutationResponse,
   GenericErrorsResponse
 } from '#testSupport/helpers/api';
-import EmailVerify from '@/pages/user/email-verify/[token]/index.vue';
 import { flushPromises } from '@vue/test-utils';
 
-describe('Email Verify', function () {
+import EmailChangeActivate from '@/pages/user/email-verify/[token]/index.vue';
+
+describe('Email Change Activate', function () {
   let component;
 
-  const mountComponent = async (apolloyResponseInner) => {
-    component = await mount(EmailVerify, {
+  const mountComponent = async (apolloResponses) => {
+    component = await mount(EmailChangeActivate, {
+      shallow: false,
       apollo: {
-        mutationResponses: [
-          GenericApolloResponse('verifyAccount', apolloyResponseInner)
-        ]
+        mutationResponses: apolloResponses
       },
       routeInfo: {
         params: {
@@ -27,21 +27,56 @@ describe('Email Verify', function () {
     });
   };
 
-  it('verifies an account with valid token', async () => {
-    await mountComponent(GenericMutationResponse());
+  it('adds secondary email with valid token', async () => {
+    await mountComponent([
+      GenericApolloResponse('verifySecondaryEmail', GenericMutationResponse())
+    ]);
 
-    expect(component.text()).to.contain('Verifying email');
+    expect(component.text()).to.contain('Adding email');
 
     await flushPromises();
+    await component.vm.$nextTick();
 
-    const router = useRouter();
-    expect(router.replace).toHaveBeenCalledWith('/login');
+    expect(component.text()).to.contain('Complete email change');
+    expect(component.findAll('input')).length(1);
   });
 
   it('shows error with invalid token', async () => {
-    await mountComponent(GenericErrorsResponse());
+    await mountComponent([
+      GenericApolloResponse('verifySecondaryEmail', GenericErrorsResponse())
+    ]);
 
     await flushPromises();
+    await component.vm.$nextTick();
+
     expect(component.text()).to.contain('There was an error');
+  });
+
+  describe('with added secondary email', () => {
+    let replaceStub;
+    beforeEach(async () => {
+      await mountComponent([
+        GenericApolloResponse(
+          'verifySecondaryEmail',
+          GenericMutationResponse()
+        ),
+        GenericApolloResponse('swapEmails', GenericMutationResponse()),
+        GenericApolloResponse('removeSecondaryEmail', GenericMutationResponse())
+      ]);
+
+      await flushPromises();
+      await component.vm.$nextTick();
+    });
+
+    it('can enter password to swap', async () => {
+      component.find('input').setValue('mypassword');
+      await component.find('form').trigger('submit');
+
+      await flushPromises();
+      await component.vm.$nextTick();
+
+      const router = useRouter();
+      expect(router.replace).toHaveBeenCalledWith('/user');
+    });
   });
 });
