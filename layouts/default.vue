71daed0fee2c9ff6d27cd78719be4fcaeff2d240
Alex Toff <me@alextoff.uk>
2023-02-26 12:08:28 +0000

    fix: Nuxt loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="flex flex-col h-screen font-body">
-    <NuxtLoadingIndicator color="#FF9F1C" :height="5" />
     <LayoutNavBar />
     <UiBreadcrumbs v-if="navStore.breadcrumbs" :crumbs="navStore.breadcrumbs" />
     <main class="flex-1 pb-2 text-white bg-sta-gray">
