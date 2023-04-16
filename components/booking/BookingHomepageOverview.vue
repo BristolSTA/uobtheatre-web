29eb131376edba4cbad3e33c9201d90faec4812f
streth11 <tom.streuli@gmail.com>
2023-04-16 10:37:48 +0000

    query and componet data

---
+++ components/booking/BookingHomepageOverview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-wrap justify-center">
+    <div
+      v-for="(booking, index) in props.bookings"
+      :key="index"
+      class="performance p-2 w-full md:w-1/2 xl:w-1/3"
+    >
+      <booking-summary-overview class="h-full" :booking="booking" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BookingSummaryOverview from '@/components/booking/overview/BookingSummaryOverview.vue';
+import { UpcomingBookingsQuery } from '~~/graphql/codegen/operations';
+
+const props = defineProps<{
+  bookings: Array<
+    NonNullable<
+      NonNullable<
+        NonNullable<UpcomingBookingsQuery['me']>['bookings']['edges'][number]
+      >['node']
+    >
+  >;
+}>();
+</script>
