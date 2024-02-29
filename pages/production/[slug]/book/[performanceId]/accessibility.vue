a0a2a8b7142cc2cf18a3dea24e4b0d499fbc9317
HappyNTH <HappyNTH@users.noreply.github.com>
2024-02-29 16:16:09 +0000

    Create an accessability stage for bookings

---
+++ pages/production/[slug]/book/[performanceId]/accessibility.vue
@@ -0,0 +1,147 @@
+<template>
+  <div v-if="booking.performance && ticketMatrix" class="text-white">
+    <BookingSelectedPerformanceBar
+      v-if="booking.performance"
+      :performance="booking.performance"
+    />
+    <div
+      v-if="booking.tickets.length"
+      class="mt-2 min-w-1/2 text-center border-4 border-dashed border-sta-gray rounded-md text-white"
+    >
+      <h2 class="text-h2">Selected Tickets</h2>
+
+      <BookingSelectedTicketsTable
+        :ticket-matrix="ticketMatrix"
+        :booking="booking"
+      />
+    </div>
+    <div class="space-y-4">
+      <UiFormLabel>
+        Accessibility Requirements
+        <template #control>
+          <UiInputText
+            v-model="booking.accessibilityInfo"
+            name="Accessibility Requirements"
+            type="accessibility"
+            placeholder="e.g. Wheelchair seat required"
+            :errors="errors"
+            required /></template
+      ></UiFormLabel>
+    </div>
+    <div v-if="booking.tickets.length" class="mt-2 text-center">
+      <button
+        class="btn btn-orange font-semibold"
+        :disabled="booking.dirty"
+        @click="
+          $emit('next-stage');
+          updateAPI();
+        "
+        @keypress="$emit('next-stage')"
+      >
+        Next
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import lo from 'lodash';
+
+import Booking from '~~/classes/Booking';
+import TicketMatrix from '~~/classes/TicketsMatrix';
+import BookingMutation from '@/graphql/mutations/booking/Booking.gql';
+import { getValidationErrors, performMutation } from '~~/utils/api';
+
+import BookingStage from '@/classes/BookingStage';
+import TicketsEditor from '@/components/booking/editor/TicketsEditor.vue';
+import { recordEvent, events } from '~~/utils/analytics';
+const stageInfo = new BookingStage({
+  name: 'Accessibility Information',
+  routeName: 'production-slug-book-performanceId-access',
+  eligable: (_, booking) => !booking.dirty && booking.tickets.length > 0
+});
+
+export default defineNuxtComponent({
+  stageInfo,
+  components: {
+    TicketsEditor
+  },
+  props: {
+    production: {
+      required: true,
+      type: Object
+    },
+    booking: {
+      required: true,
+      type: Booking
+    },
+    ticketMatrix: {
+      type: TicketMatrix,
+      default: null
+    }
+  },
+  data() {
+    return {
+      interaction_timer: lo.debounce(this.updateAPI, 2 * 1000),
+      errors: null
+    };
+  },
+  mounted() {
+    this.$emit('mounted', stageInfo);
+  },
+  methods: {
+    async updateAPI() {
+      let bookingResponse;
+      try {
+        if (!this.booking.id) {
+          // We haven't got a booking yet, lets create one
+          const data = await performMutation(
+            this.$apollo,
+            {
+              mutation: BookingMutation,
+              variables: {
+                input: {
+                  performance: this.booking.performance.id,
+                  tickets: this.booking.toAPIData().tickets,
+                  accessibilityInfo: this.booking.accessibilityInfo
+                }
+              }
+            },
+            'booking'
+          );
+          bookingResponse = data.booking.booking;
+          recordEvent(events.booking.started);
+        } else {
+          // We have a booking, lets update it
+          const data = await performMutation(
+            this.$apollo,
+            {
+              mutation: BookingMutation,
+              variables: {
+                input: {
+                  id: this.booking.id,
+                  tickets: this.booking.toAPIData().tickets,
+                  accessibilityInfo: this.booking.accessibilityInfo
+                }
+              }
+            },
+            'booking'
+          );
+          bookingResponse = data.booking.booking;
+        }
+      } catch (e) {
+        this.errors = getValidationErrors(e);
+        return;
+      }
+
+      // Check for changes since API called.
+      if (this.booking.tickets.length === bookingResponse.tickets.length) {
+        return this.booking.updateFromAPIData(bookingResponse);
+      }
+
+      // There has been a change in the selected tickets whilst calling the API. Let's trigger another call...
+      this.interaction_timer();
+    }
+  }
+});
+</script>
