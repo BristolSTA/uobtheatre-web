39314d6dbc15a1a0abffcf77e39c1ba5a2057f73
HappyNTH <HappyNTH@users.noreply.github.com>
2024-03-02 15:12:24 +0000

    Add a button to edit accessibility info

--- components/booking/overview/AccessibilityOverview.vue
+++ components/booking/overview/AccessibilityOverview.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-mutating-props -->
 <template>
   <overview-box>
     <template #title>
@@ -5,7 +6,48 @@
       Accessibility Info
     </template>
     <div class="p-2 px-4 bg-sta-gray rounded">
-      {{ booking.accessibilityInfo }}
+      <template v-if="!changingAccessibility">
+        <div class="flex">
+          <p class="flex-grow py-2">
+            {{ booking.accessibilityInfo }}
+          </p>
+          <UiStaButton
+            class="bg-sta-orange hover:bg-sta-orange-dark transition-colors lg:mx-8 mx-4"
+            @click="
+              () => {
+                changingAccessibility = true;
+                newAccessibility = booking.accessibilityInfo;
+              }
+            "
+          >
+            Edit
+          </UiStaButton>
+        </div>
+      </template>
+      <template v-else>
+        <div class="flex">
+          <form-label class="flex-grow">
+            <UiInputText
+              v-model="newAccessibility"
+              placeholder="e.g. Wheelchair seat required"
+              :errors="errors"
+              required
+            />
+          </form-label>
+          <UiStaButton
+            class="bg-sta-green hover:bg-sta-green-dark transition-colors lg:mx-8 mx-4"
+            @click="
+              () => {
+                booking.accessibilityInfo = newAccessibility;
+                updateAPI();
+                changingAccessibility = false;
+              }
+            "
+          >
+            Save
+          </UiStaButton>
+        </div>
+      </template>
     </div>
   </overview-box>
 </template>
@@ -13,14 +55,50 @@
 <script>
 import OverviewBox from '../../ui/UiCard.vue';
 import Booking from '~~/classes/Booking';
+import BookingMutation from '@/graphql/mutations/booking/Booking.gql';
+import { getValidationErrors, performMutation } from '~~/utils/api';
+
+import FormLabel from '../../ui/FormLabel.vue';
 
 export default {
   name: 'TicketsOverview',
-  components: { OverviewBox },
+  components: { OverviewBox, FormLabel },
   props: {
     booking: {
       required: true,
       type: Booking
+    }
+  },
+  data() {
+    return {
+      changingAccessibility: false,
+      errors: null
+    };
+  },
+  methods: {
+    async updateAPI() {
+      try {
+        if (this.booking.id) {
+          // We have a booking, lets update it
+          const data = await performMutation(
+            this.$apollo,
+            {
+              mutation: BookingMutation,
+              variables: {
+                input: {
+                  id: this.booking.id,
+                  accessibilityInfo: this.booking.accessibilityInfo
+                }
+              }
+            },
+            'booking'
+          );
+          this.booking.updateFromAPIData(data.booking.booking);
+        }
+      } catch (e) {
+        this.errors = getValidationErrors(e);
+        return;
+      }
     }
   }
 };
