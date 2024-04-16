3ec2508eb4b2792cf6ad1e17b2be33529198d134
HappNTH <crayner.auburn@yahoo.co.uk>
2024-04-16 09:20:11 +0000

    Add in future show carousel, further styling

--- pages/venue/[slug]/index.vue
+++ pages/venue/[slug]/index.vue
@@ -11,26 +11,57 @@
         'background-image': banner
       }"
     />
-    <div class="grid gap-4 grid-cols-1 lg:grid-cols-5 lg:grid-rows-5">
-      <div
-        class="lg:col-start-1 lg:col-span-3 lg:row-start-1 bg-sta-gray-light"
-      >
+    <div
+      class="container grid gap-4 p-4 grid-cols-1 lg:grid-cols-5"
+      style="grid-template-rows: auto auto auto"
+    >
+      <div class="lg:col-start-1 lg:col-span-5 lg:row-start-1">
         <!-- Title -->
-        <h1 class="container py-6 text-left text-h1">
+        <h1 class="container align-middle py-6 text-left text-h1">
           {{ venue.name }}
         </h1>
       </div>
-      <div
-        class="lg:col-start-1 lg:col-span-3 lg:row-start-2 lg:row-span-2 bg-sta-rouge"
-      >
-        <!-- Show Carrosel -->
-      </div>
-      <div class="lg:col-start-4 lg:col-span-2 lg:row-start-1 lg:row-span-5">
-        <!-- Banner Information Section -->
+      <div class="lg:col-start-1 lg:col-span-3 lg:row-start-2">
         <div
-          class="flex flex-col flex-wrap items-center justify-center bg-sta-gray-dark"
+          v-if="!bannerProductions.length"
+          class="flex items-center bg-black bg-opacity-40"
+          style="min-height: 20vh"
         >
-          <div class="flex justify-center p-4 w-11/12 bg-sta-gray-light">
+          <div class="container px-4 text-white text-center text-2xl lg:w-2/3">
+            No Upcoming Productions
+          </div>
+        </div>
+        <UiCarousel v-else: :carousel-items="bannerProductions">
+          <template #default="slotProps">
+            <div class="flex items-center h-full bg-black bg-opacity-40">
+              <NuxtLink
+                class="container px-4 md:pl-12 lg:pl-4 lg:w-2/3"
+                :to="`/production/${slotProps.carouselItem.text.slug}`"
+              >
+                <div class="text-2xl">
+                  {{ slotProps.carouselItem.text.society.name }}
+                </div>
+                <div class="text-h1">
+                  {{ slotProps.carouselItem.text.name }}
+                </div>
+                <div class="text-2xl">
+                  {{
+                    displayStartEnd(
+                      slotProps.carouselItem.text.start,
+                      slotProps.carouselItem.text.end,
+                      'd MMMM'
+                    )
+                  }}
+                </div>
+              </NuxtLink>
+            </div>
+          </template>
+        </UiCarousel>
+      </div>
+      <div class="lg:col-start-4 lg:col-span-2 lg:row-start-2 lg:row-span-2">
+        <!-- Banner Information Section -->
+        <div class="flex flex-col flex-wrap items-center justify-center">
+          <div class="flex justify-center w-full p-4 bg-sta-gray-light">
             <div>
               <h2 class="text-sta-orange text-3xl font-semibold">
                 Venue Info:
@@ -55,7 +86,8 @@
                           {{ venue.address.street }}
                         </p>
                         <p>
-                          {{ venue.address.city }}, {{ venue.address.postcode }}
+                          {{ venue.address.city }},
+                          {{ venue.address.postcode }}
                         </p>
                       </div>
                     </td>
@@ -75,7 +107,7 @@
             v-if="
               venue.address && venue.address.latitude && venue.address.longitude
             "
-            class="flex justify-center w-11/12 h-96 lg:mb-4"
+            class="flex justify-center w-full h-96"
           >
             <UiMap
               v-if="venue !== null"
@@ -84,25 +116,61 @@
               @initalised="initMap"
             />
           </div>
-          <div class="flex flex-wrap items-center justify-center">
-            <VenueAccessibility :venue-data="venue" />
+          <div class="flex justify-center w-full p-4 bg-sta-gray-light">
+            <div>
+              <h2 class="text-sta-orange text-3xl font-semibold">
+                Accessibility Info:
+              </h2>
+              <div class="mt-2 text-white">
+                <loading-icon v-if="!venue" />
+                <p v-else-if="venue.accessibilityInfo">
+                  {{ venue.accessibilityInfo }}
+                </p>
+                <p v-else class="font-semibold">
+                  No accessibility information available for this venue
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div
-        class="lg:col-start-1 lg:col-span-2 lg:row-start-4 lg:row-span-2 bg-sta-orange flex flex-wrap items-center justify-center"
-      >
+      <div class="lg:col-start-1 lg:col-span-3 lg:row-start-3">
         <!-- Description -->
         <UiTipTapOutput
-          class="px-6 w-full text-justify lg:block lg:px-2 lg:w-1/2"
+          class="p-4 w-full text-justify lg:block"
           :html="venue.description"
         />
       </div>
       <div
-        class="lg:col-start-3 lg:row-start-4 lg:row-span-2 bg-sta-green flex flex-wrap items-center justify-center"
+        v-if="pastProductions.length"
+        class="lg:col-start-1 lg:col-span-3 lg:row-start-4"
       >
         <!-- Past Shows -->
-        <p>Past Shows Here</p>
+        <div ref="production-list" class="flex-none container">
+          <div class="w-full bg-sta-gray-dark">
+            <h2 class="flex justify-center mb-2 text-2xl">Past Productions</h2>
+            <table class="table-auto w-full">
+              <tbody>
+                <tr
+                  v-for="(production, index) in pastProductions"
+                  :key="index"
+                  class="even:bg-sta-gray odd:bg-sta-gray-light"
+                >
+                  <td
+                    class="pl-4 py-2 hover:text-gray-300 text-xl font-semibold"
+                  >
+                    <NuxtLink :to="`/production/${production.slug}`">
+                      {{ production.name }}
+                    </NuxtLink>
+                  </td>
+                  <td class="px-4 text-right">
+                    {{ dateFormat(production.end, 'MMMM y') }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -136,13 +204,36 @@ export default defineNuxtComponent({
       });
     }
 
+    const upcomingProductions = venue.productions
+      ? venue.productions.edges.map((edge) => edge.node)
+      : [];
+
+    const bannerProductions = upcomingProductions
+      .filter((production) => production?.coverImage)
+      .map((production) => {
+        return {
+          id: production.id,
+          displayImage: production.coverImage,
+          text: {
+            slug: production.slug,
+            name: production.name,
+            start: production.start,
+            end: production.end,
+            society: production.society
+          }
+        };
+      })
+      .slice(0, 4);
+
     return {
-      venue
+      venue,
+      bannerProductions
     };
   },
   data() {
     return {
-      venue: null
+      venue: null,
+      bannerProductions: null
     };
   },
   methods: {
@@ -177,6 +268,16 @@ export default defineNuxtComponent({
           ? `,${this.venue.address.street},${this.venue.address.city}`
           : '')
       );
+    },
+    productions() {
+      return this.venue.productions.edges
+        .map((edge) => edge.node)
+        .filter((production) => production.end);
+    },
+    pastProductions() {
+      return this.venue.productions.edges
+        .map((edge) => edge.node)
+        .filter((production) => production.end);
     }
   }
 });
