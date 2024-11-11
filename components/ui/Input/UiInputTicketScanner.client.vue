fa1d37bbce1ee37a7bbd272066770a2b4a61373e
Zack Colton <zack@familycolton.co.uk>
2024-11-11 16:47:27 +0000

    Fix ticket scanner loading

--- components/ui/Input/UiInputTicketScanner.client.vue
+++ components/ui/Input/UiInputTicketScanner.client.vue
@@ -2,8 +2,8 @@
   <qrcode-stream
     :camera="!on || cameraReset ? 'off' : 'auto'"
     :track="onTrackEvent"
-    @init="onInit"
-    @decode="onDecode"
+    @camera-on="cameraOn"
+    @detect="onDetect"
   >
     <slot />
   </qrcode-stream>
@@ -56,7 +56,7 @@ function onTrackEvent(detectedCodes: any, ctx: any) {
   }
 }
 
-async function onInit(promise: Promise<any>) {
+async function cameraOn(promise: Promise<any>) {
   try {
     await promise;
     emit('ready');
@@ -90,7 +90,7 @@ async function onInit(promise: Promise<any>) {
     emit('unable', errorMessage);
   }
 }
-function onDecode(string: string) {
+function onDetect(string: string) {
   new Audio('/audio/beep_single.mp3').play();
 
   try {
