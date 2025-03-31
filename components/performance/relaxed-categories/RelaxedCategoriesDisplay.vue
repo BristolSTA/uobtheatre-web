d67d34cad0f30016c72445e3fd9503d3d5ca4788
Finn <es24117@bristol.ac.uk>
2025-03-31 23:29:05 +0100

    show categories on production page

---
+++ components/performance/relaxed-categories/RelaxedCategoriesDisplay.vue
@@ -0,0 +1,44 @@
+<template>
+  <modal
+    @close="$emit('close')"
+  >
+    <div class="lg:w-1/3-screen">
+      <h2 class="text-lg font-semibold px-3">
+        {{ thisRelaxedName }} Performance
+      </h2>
+      <p class="p-3">
+        This performance has been adapted to be more accessible to different
+        audience members. The following adaptations have been made:
+      </p>
+
+      <ul class="gap-y-2 flex flex-col">
+        <li v-for="rc in relaxedCategories" :key="rc.id">
+          <relaxed-category-display :relaxed-category="rc" />
+        </li>
+        <li v-if="relaxedCategories.length === 0" ref="no-categories" class="w-full px-3 text-sta-rouge font-bold">
+          No further information is available.
+        </li>
+      </ul>
+    </div>
+  </modal>
+</template>
+
+<script>
+import RelaxedCategoryDisplay from './RelaxedCategoryDisplay.vue';
+import Modal from '~/components/ui/Modal.vue';
+export default {
+  components: { Modal, RelaxedCategoryDisplay },
+  props: {
+    relaxedCategories: {
+      required: true,
+      type: Array
+    },
+    thisRelaxedName: {
+      required: true,
+      type: String
+    }
+  },
+
+  emits: ['close']
+};
+</script>
