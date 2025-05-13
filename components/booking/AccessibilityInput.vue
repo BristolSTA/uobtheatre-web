48cf6145ccdb3b92ed0a4f5d90cebf568305ce49
HappNTH <crayner.auburn@yahoo.co.uk>
2025-05-13 21:20:46 +0000

    Show previous accessibility information for admins

--- components/booking/AccessibilityInput.vue
+++ components/booking/AccessibilityInput.vue
@@ -24,11 +24,11 @@
       /></template>
       <template #helper>
         Any accessibility requirements you chose to share with us will be made
-        available via email and the website to the production team and website
-        staff. Any information you provide will be deleted shortly after the
-        event. We cannot guarantee that your requirements can be met, though a
-        member of the team will be in touch to discuss your requirements if
-        needed. If you have any questions, please email
+        available to the production team and website staff. Any information you
+        provide will be deleted shortly after the event. We cannot guarantee
+        that your requirements can be met, though a member of the team will be
+        in touch to discuss your requirements if needed. If you have any
+        questions, please email
         <a href="mailto:support@uobtheatre.com" class="underline">
           support@uobtheatre.com</a
         >.
