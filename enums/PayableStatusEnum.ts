28369807a90c0f86987ac1aa5d39332b8cddd3ed
HappNTH <crayner.auburn@yahoo.co.uk>
2025-05-14 13:21:26 +0000

    Support displaying refunding and refunded tickets, fix a table issue

--- enums/PayableStatusEnum.ts
+++ enums/PayableStatusEnum.ts
@@ -4,6 +4,8 @@ export default class extends BaseEnum {
   override nameMap = {
     IN_PROGRESS: 'In Progress',
     CANCELLED: 'Cancelled',
-    PAID: 'Paid'
+    PAID: 'Paid',
+    REFUND_PROCESSING: 'Refund Processing',
+    REFUNDED: 'Refunded'
   };
 }
