3582e3f7ec81c48dc35097fcea994098af28d374
HappNTH <crayner.auburn@yahoo.co.uk>
2025-09-05 12:13:45 +0000

    more nonsense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ const cssFiles = [
 ];
 
 // If we are NOT testing (cypress), we push the app styles
-if (process.env.MODE !== 'test') cssFiles.push('@/assets/styles/app.css');
+if (process.env.MODE !== 'test') cssFiles.push('@/assets/css/app.css');
 
 export default defineNuxtConfig({
   // Enable experimental features
