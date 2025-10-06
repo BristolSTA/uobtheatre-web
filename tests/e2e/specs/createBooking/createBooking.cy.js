db03384e9e21be3fa9e2002fe32c9841c87e7f12
zst-c <zack@familycolton.co.uk>
2025-10-06 13:19:12 +0100

    Fix e2e tests with new db data

--- tests/e2e/specs/createBooking/createBooking.cy.js
+++ tests/e2e/specs/createBooking/createBooking.cy.js
@@ -1,14 +1,14 @@
 describe('Create Booking Process', () => {
   it('redirects away unauthenticated user', () => {
-    cy.visit('/production/legally-blonde/book/UGVyZm9ybWFuY2VOb2RlOjE=');
+    cy.visit('/production/aurora-nights/book/UGVyZm9ybWFuY2VOb2RlOjI=');
     cy.url().should('match', /login/);
   });
   it('doesnt redirect away authenticated user', () => {
     cy.login();
-    cy.visit('/production/legally-blonde/book/UGVyZm9ybWFuY2VOb2RlOjE=');
+    cy.visit('/production/aurora-nights/book/UGVyZm9ybWFuY2VOb2RlOjI=');
     cy.url().should(
       'match',
-      /production\/legally-blonde\/book\/UGVyZm9ybWFuY2VOb2RlOjE=/
+      /production\/aurora-nights\/book\/UGVyZm9ybWFuY2VOb2RlOjI=/
     );
   });
 });
