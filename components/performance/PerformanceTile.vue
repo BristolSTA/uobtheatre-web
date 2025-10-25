9686d6e5b04358fc932ae7da1f0ba31ea85a781d
Finn <185564612+finn768@users.noreply.github.com>
2025-10-25 01:19:43 +0100

    make button more button

--- components/performance/PerformanceTile.vue
+++ components/performance/PerformanceTile.vue
@@ -26,29 +26,13 @@
         inc. interval
       </template>
     </div>
-    <button
-      v-if="performance.isRelaxed"
-      ref="categories"
-      class="text-left"
-      @click="showRelaxedCategoriesDetail = true"
-    >
-      <span class="hover:text-gray-200">{{ thisRelaxedName }} Performance</span>
-    </button>
-    <relaxed-categories-display
-      v-if="showRelaxedCategoriesDetail"
-      :relaxed-categories="
-              performance.relaxedCategories"
-      :this-relaxed-name="thisRelaxedName"
-      @close="showRelaxedCategoriesDetail = false"
-    />
-    <div class="flex-grow" />
-    <div class="text-sm font-semibold">
+    <div class="text-sm font-semibold mt-2">
       <p v-if="!performance.isBookable">No Tickets Available</p>
       <p v-else>Tickets Available</p>
     </div>
     <button
       v-if="!performance.isBookable"
-      class="btn btn-rouge btn-outline disabled mt-4 w-2/3 text-center font-semibold"
+      class="btn btn-rouge btn-outline disabled mt-4 text-center font-semibold"
       disabled
     >
       {{ disabledReason }}
@@ -56,13 +40,34 @@
     <component
       :is="actionPath ? 'NuxtLink' : 'button'"
       v-else
-      class="btn btn-orange mt-4 w-2/3 text-center font-semibold"
+      class="btn btn-orange mt-4 text-center font-semibold"
       :to="actionPath"
       @click="onAction"
       @keypress="onAction"
     >
-      <slot name="select-button"> Book </slot>
+      <slot name="select-button">Book</slot>
     </component>
+    <div
+      v-if="performance.isRelaxed"
+      class="-mx-3 -mb-3 mt-3 p-3 bg-sta-green-dark"
+    >
+      <button
+        ref="categories"
+        class="text-center font-semibold w-full text-gray-50 hover:text-gray-200"
+        @click="showRelaxedCategoriesDetail = true"
+      >
+        <span class="">This is a {{ thisRelaxedName }} Performance</span>
+        <span class="inline-block mr-3">More information</span>
+        <font-awesome-icon icon="chevron-right" class="inline-block" />
+      </button>
+      <relaxed-categories-display
+        v-if="showRelaxedCategoriesDetail"
+        :relaxed-categories="performance.relaxedCategories"
+        :this-relaxed-name="thisRelaxedName"
+        @close="showRelaxedCategoriesDetail = false"
+      />
+    </div>
+    <div v-else class="bg-sta-gray mt-3 -mx-3 -mb-3 flex-grow"></div>
   </div>
 </template>
 
@@ -70,12 +75,14 @@
 import { humanDuration, dateFormat } from '@/utils/datetime';
 import Modal from '~/components/ui/Modal.vue';
 import RelaxedCategoriesDisplay from '~/components/performance/relaxed-categories/RelaxedCategoriesDisplay.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default {
   name: 'PerformanceOverviewTile',
   components: {
+    FontAwesomeIcon,
     Modal,
-    RelaxedCategoriesDisplay,
+    RelaxedCategoriesDisplay
   },
   props: {
     performance: {
@@ -101,7 +108,9 @@ export default {
       return 'Unavailable';
     },
     thisRelaxedName() {
-      return this.performance.relaxedName ? this.performance.relaxedName : 'Relaxed';
+      return this.performance.relaxedName
+        ? this.performance.relaxedName
+        : 'Relaxed';
     }
   },
   methods: {
